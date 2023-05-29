b1e5be7e6ce8b834b1c86e902c8c7e6e3f2268ab
bea-menezes <beatrizprs.menezes@gmail.com>
2023-05-29 16:20:13 -0300

    tela de login

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function onPart1() {
+    var part1 = document.querySelector('.register-part-1');
+    var part2 = document.querySelector('.register-part-2');
+    var btn = document.querySelector('#btn-back');
+    part1.style.display = 'none';
+    part2.style.display = 'block';
+    btn.style.display = 'block';
+}
+
+function onPart2() {
+    var part1 = document.querySelector('.register-part-1');
+    var part2 = document.querySelector('.register-part-2');
+    var btn = document.querySelector('#btn-back');
+    part1.style.display = 'block';
+    part2.style.display = 'none';
+    btn.style.display = 'none';
+}
+
+
